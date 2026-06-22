7b27da395817c290fae4902ea4555e0e6eec663c
Paweł Dubowik <p.dubowik27@gmail.com>
2026-06-23 00:22:34 +0200

    Generate modules, services and controllers for orders and products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
