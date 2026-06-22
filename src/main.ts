50085e299bf8993d707c1b502a0b6d19ea11f97f
Paweł Dubowik <p.dubowik27@gmail.com>
2026-06-23 00:07:41 +0200

    Implement Prisma and connect to the database. Create models for order, product, image, and orderItem

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  await app.enableShutdownHooks();
+
+  await app.listen(8000);
 }
 bootstrap();
