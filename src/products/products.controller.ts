937f3f805d5745ed6df9669d37077287d60a4b32
Paweł Dubowik <p.dubowik27@gmail.com>
2026-07-03 17:45:04 +0200

    Fix expected values in products.controller.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Product } from '@prisma/client';
 import { ParseUUIDPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
@@ -7,12 +8,12 @@ export class ProductsController {
     constructor(private productsService: ProductsService) {}
 
     @Get('/')
-    getAll(): any {
+    getAll(): Promise<Product[]> {
         return this.productsService.getAll();
     }
 
     @Get('/:id')
-    async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    async getById(@Param('id', new ParseUUIDPipe()) id: string): Promise<Product> {
         const product = await this.productsService.getById(id);
         if(!product) throw new NotFoundException('Product not found')
         return product;
