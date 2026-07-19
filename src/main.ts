3cc59454352deaf70ae0e38f798884216d17bbe3
Paweł Dubowik <p.dubowik27@gmail.com>
2026-07-19 17:46:49 +0200

    Add fallback to main.ts to fix routing.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,11 +11,24 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:8000',
       'https://proffee-bm1j.onrender.com'
     ],
   });
 
   await app.enableShutdownHooks();
+
+  app.use((req, res, next) => {
+    if(
+      !req.url.startsWith('/api') &&
+      !req.url.startsWith('/static') &&
+      !req.url.includes('.')
+    ) {
+      res.sendFile(join(__dirname, '../../client/build/index.html'));
+    } else {
+      next();
+    }
+  });
 
   await app.listen(8000);
 }
