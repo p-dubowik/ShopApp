ac5169e55c861273cefbe6b47a2c9b6a047cf815
Paweł Dubowik <p.dubowik27@gmail.com>
2026-07-19 14:46:47 +0200

    Prepare site for deploy. Change db provider to postgre.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [OrdersModule, ProductsModule],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '../../', 'client', 'build') }), 
+    OrdersModule,
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
